9b65d4908c634e97761246068ee293b966a4815c
Vins0n <vinsonring@gmail.com>
2016-04-07 09:29:14 +0800

    update

--- snippets/models.py
+++ snippets/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
 # Create your models here.
+
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    code = models.TextField()
+    linenos = models.BooleanField(default=False)
+    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
+    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+
+    class Meta:
+        ordering = ('created',)
